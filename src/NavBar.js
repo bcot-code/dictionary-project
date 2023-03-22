567356d568f15c2786c779b7767921fc92bb11f0
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-22 12:03:47 -0500

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,22 +4,19 @@ export default function NavBar() {
   return (
     <ul className="nav position-absolute top-0 end-0">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">
+        <a className="nav-link active" aria-current="page" href="#top">
           Active
         </a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a className="nav-link" href="#top">
           Link
         </a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a className="nav-link" href="#top">
           Link
         </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled">Disabled</a>
       </li>
     </ul>
   );
