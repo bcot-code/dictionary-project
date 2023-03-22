a8177f27590e43665c884999d0b191664428827b
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-22 12:38:55 -0500

    Added ApI

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setWork] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let api = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=db9df5cafd32a54ef0cf0c7bd2635oct`;
+    console.log(api);
+    axios.get(api).then(handleResponse);
   }
 
   function handleKey(event) {
